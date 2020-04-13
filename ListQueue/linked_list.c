0e8fd7b026c25310e0d8749139ce4068d67b620e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-13 09:17:35 +0300

    add function that add node to the end of the linked list

--- ListQueue/linked_list.c
+++ ListQueue/linked_list.c
@@ -50,3 +50,52 @@ void free_list(node_in_list* head) {
 		free(tmp);
 	}
 }
+
+node_in_list* add_node_to_end_of_list(node_in_list* head) {
+	/********************************************************\
+	* Function name - add_node_to_end_of_list
+	*
+	* Function Purpose - add new node to the end of the list
+	*					 if the list is don't have elements than the head
+	*					 will be the new node
+	*
+	* Parameters - INOUT node_in_list* head - the first node in the linked list
+	*										in the function add to it the new
+	*										node in the end of the list
+	*
+	* Return Value - the list after add to it new node in the end
+	*
+	* Side Effects - this function allocate memory for the new element
+	*				 and add it to the list. if the list is empty than the \
+	*				 head will be the new element
+	*
+	* Semantics - this function allocate memory for the new element
+	*			  and add it to the list. if the list is empty than the \
+	*			  head will be the new element. if allocation failed return NULL
+	*
+	* Author - Liri
+	\********************************************************/
+	node_in_list *new_node;
+	node_in_list *current_node;
+	new_node = (node_in_list*)malloc(sizeof(node_in_list));
+	if (NULL == new_node) {
+		printf(ERROR_MEMORY_ALLOCATION_FAILED);
+		return NULL;
+	}
+	new_node->next = NULL;
+
+	if (head == NULL) {
+		return new_node;
+	}
+
+	else {
+		/*add the new node in the end of the list*/
+		current_node = head;
+		while (NULL != current_node->next)
+		{
+			current_node = current_node->next;
+		}
+		current_node->next = new_node;
+	}
+	return head;
+}
--- ListQueue/linked_list.h
+++ ListQueue/linked_list.h
@@ -21,3 +21,4 @@ typedef struct list
 
 node_in_list* initialize_list();
 void free_list(node_in_list* head);
+node_in_list* add_node_to_end_of_list(node_in_list* head);

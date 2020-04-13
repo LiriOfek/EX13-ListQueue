8a93ae89eabd59e9716bedb810c70b5ba7980d8b
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-13 09:55:28 +0300

    add documentation

--- ListQueue/linked_list.c
+++ ListQueue/linked_list.c
@@ -1,5 +1,5 @@
 /********************************************************\
-File Name: linked_list.h
+File Name: linked_list.c
 Author: Liri (13/4/2020)
 Purpose: This file contain the the functions for implement
 		 linked list

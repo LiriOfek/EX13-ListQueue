ed2e581e24cc2f0eacf278213e91c438122ed542
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-13 09:15:15 +0300

    add function that initialize the linked list

--- ListQueue/linked_list.c
+++ ListQueue/linked_list.c
@@ -1,6 +1,22 @@
-/********************************************************\
-File Name: linked_list.c
-Author: Liri (13/4/2020)
-Purpose: This file contain the the functions for implement
-		 linked list
-\********************************************************/
+#include "linked_list.h"
+
+node_in_list* initialize_list() {
+	/********************************************************\
+	* Function name - initialize_list
+	*
+	* Function Purpose - initialize the linked list
+	*
+	* Parameters - INOUT linked_list* head - pointer to the first node
+	*				in linked list, initiate it with NULL
+	*
+	* Return Value - the pointer to the first node in linkes list
+	*
+	* Side Effects - this function point the head of the list to NULL
+	*
+	* Semantics - this function initialize the head of the list to NULL
+	*
+	* Author - Liri
+	\********************************************************/
+	node_in_list* head = NULL;
+	return head;
+}
--- ListQueue/linked_list.h
+++ ListQueue/linked_list.h
@@ -4,3 +4,19 @@ Author: Liri (13/4/2020)
 Purpose: This file contain the the functions for implement
 		 linked list
 \********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR_MEMORY_ALLOCATION_FAILED ("Error! memory allocation failed.\n")
+#define INITIAL_LENGTH (0)
+
+/*linked list struct, containing void pointer of the element
+and the pointer to the next element in list*/
+typedef struct list
+{
+	void* element;
+	struct list *next;
+}node_in_list;
+
+node_in_list* initialize_list();

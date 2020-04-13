2061d5d9b8bb67352cd50036f3213c06fb699cb0
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-13 09:12:33 +0300

    add files that contain the implementation of linked list

---
+++ ListQueue/linked_list.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: linked_list.c
+Author: Liri (13/4/2020)
+Purpose: This file contain the the functions for implement
+		 linked list
+\********************************************************/

5dc95647c4ecd6c7e31aedc06c5b74c74223ceb1
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-13 13:10:36 +0300

    add queue files with documentation

---
+++ ListQueue/queue.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: queue.c
+Author: Liri (13/4/2020)
+Purpose: This file contain the the functions for implement
+		 queue
+\********************************************************/
---
+++ ListQueue/queue.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: linked_list.h
+Author: Liri (13/4/2020)
+Purpose: This header file contain queue struct and
+		 the decleration functions for implement queue
+\********************************************************/
